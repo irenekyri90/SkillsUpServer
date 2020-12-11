ebd63d0f929ee581e520e5bb30dc596408aaf76d
Lucia Skudlik <lucia.skudlik@gmail.com>
2020-12-11 21:23:23 +0100

    implement cloudinary for workshop route

--- routes/workshop.router.js
+++ routes/workshop.router.js
@@ -4,7 +4,28 @@ const router = express.Router();
 const {isLoggedIn} = require("./../helpers/middlewares");
 const User = require("./../models/user.model");
 
+const uploader = require("./../config/cloudinary-setup");
+
 const Workshop = require('../models/workshop.model');
+
+
+// include CLOUDINARY:
+//upload a single image per once.
+// ADD an horitzontal middleware
+router.post("/upload", uploader.single("img"), (req, res, next) => {
+  console.log("file is: ", req.file);
+
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  // get secure_url from the file object and save it in the
+  // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
+  res.json({ secure_url: req.file.secure_url });
+});
+
+
+
 
 //POST 'api/workshops'  => to post a new workshop
 
