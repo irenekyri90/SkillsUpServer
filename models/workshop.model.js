a67355a0d1383130ed76cf58e0055b7b51cb545e
Lucia Skudlik <lucia.skudlik@gmail.com>
2020-12-12 12:35:21 +0100

    require workshop.model fields

--- models/workshop.model.js
+++ models/workshop.model.js
@@ -3,16 +3,16 @@ const Schema = mongoose.Schema;
 
 const workshopSchema = new Schema({
     title: {type: String, required: true},
-    img: {type: String},
+    img: {type: String, required: true},
     description: {type: String, required: true},
-    category: {type: String},
-    date: {type: Date},
+    category: {type: String, required: true},
+    date: {type: Date, required: true},
     length: {type: Number, required: true},
     credits: {type: Number, required: true},
     maxParticipants: {type: Number, required: true},
     participants: [{type: Schema.Types.ObjectId, ref:'User'}],
     host: {type: Schema.Types.ObjectId,ref:'User', default: null},
-    location: {type: String}  
+    location: {type: String, required: true}  
 }, {
   timestamps: {
     createdAt: 'created_at',
