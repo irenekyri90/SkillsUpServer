f31dc29f806d02034c7665b8e6eeaccccaa4d638
Irene Kyritsis <irene.k@live.ca>
2020-12-14 22:43:43 +0100

    populate host

--- routes/workshop.router.js
+++ routes/workshop.router.js
@@ -179,6 +179,7 @@ router.get('/workshops/:id', (req, res) => {
 
   Workshop.findById(id)
     .populate("participants")
+    .populate("host")
     .then((foundWorkshop) => {
       
       res.status(200).json(foundWorkshop);
